a0e60567d714472b9b2f91e607d71e3ecaad90e9
KoTiK <temhbz@mail.ru>
2022-06-06 21:38:36 +0700

    4 экрана и все переходы

---
+++ app/src/main/java/com/example/pr12_kargin/Start_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.pr12_kargin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_screen)
+    }
+}
