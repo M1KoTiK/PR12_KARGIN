6b4b469baa833f34411b8c45e2756c981aa57bd5
KoTiK <temhbz@mail.ru>
2022-06-06 19:22:54 +0700

    init

---
+++ app/src/main/java/com/example/pr12_kargin/Registration.kt
@@ -0,0 +1,16 @@
+package com.example.pr12_kargin
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class Registration : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+
+    }
+}
