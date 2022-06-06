275257aa6590de04263ca34ea7cae88aa3ccb517
KoTiK <temhbz@mail.ru>
2022-06-06 23:15:44 +0700

    добавлены обратные переходы

--- app/src/main/java/com/example/pr12_kargin/Menus.kt
+++ app/src/main/java/com/example/pr12_kargin/Menus.kt
@@ -1,11 +1,26 @@
 package com.example.pr12_kargin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 
 class Menus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menus)
+
+        val itemHistory = findViewById<LinearLayout>(R.id.itemHistory)
+        itemHistory.setOnClickListener{
+            val openHistory = Intent(this, History::class.java)
+            startActivity(openHistory)
+        }
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+        btnBack.setOnClickListener{
+            val openStart_Screen = Intent(this, Start_Screen::class.java)
+            startActivity(openStart_Screen)
+        }
     }
 }
